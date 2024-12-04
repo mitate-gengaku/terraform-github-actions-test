8af35b8f36ef1aed3547a1ef1db67fea3ad2e6a2
mitate-gengaku <mitategangaku@gmail.com>
2024-12-04 14:28:37 +0900

    fix: fix actions

--- dev/variables.tf
+++ dev/variables.tf
@@ -11,49 +11,49 @@ variable "image_acm_domain" {
 }
 
 variable "dbpassword_name" {
-  
+  type = string
 }
 
 variable "db_name" {
-  
+  type = string
 }
 
 variable "rds_username_paramete_name" {
-  
+  type = string
 }
 
 variable "rds_dbname_parameter_name" {
-  
+  type = string
 }
 
 variable "rds_parameter_name" {
-  
+  type = string
 }
 
 variable "route53_domain_name" {
-  
+  type = string
 }
 
 variable "bucket_source_arn" {
-  
+  type = string
 }
 
 variable "env_s3" {
-  
+  type = string
 }
 
 variable "laravel_container_image" {
-  
+  type = string
 }
 
 variable "nginx_container_image" {
-  
+  type = string
 }
 
 variable "execution_role_arn" {
-  
+  type = string
 }
 
 variable "task_role_arn" {
-  
+  type = string
 }
